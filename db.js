2ac04e2aaf2c4ea7304db4614980c3a4650a927f
NURIJE <31239598+NURIJE@users.noreply.github.com>
2021-01-06 01:28:45 +0900

    expressJS finish

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 432359,
+        title: "video awesome",
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 12121,
+            name: "nuri",
+            email: "nuri@gmail.com",
+        }
+    },
+    {
+        id: 123455,
+        title: "video goodlooking",
+        description: 'This is something I loveeeeeee!',
+        views: 56,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 69776,
+            name: "nuri",
+            email: "nuri@gmail.com",
+        }
+    },
+    {
+        id: 231255,
+        title: "video super",
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 12121,
+            name: "nuri",
+            email: "nuri@gmail.com",
+        }
+    },
+    {
+        id: 4534677,
+        title: "video nice",
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 12121,
+            name: "nuri",
+            email: "nuri@gmail.com",
+        }
+    }
+]
